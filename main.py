093fc906aee19f4e0460dad3238366082e244b29
yourash <y.anton.ua@gmail.com>
2016-10-09 23:11:32 +0300

    Add daily report

--- main.py
+++ main.py
@@ -5,19 +5,25 @@ import json,time
 access_token=''
 count_of_posts=50
 #put '-' before group id example: group_id='-23242342'
-#пишите '-' перед номером группы пример: group_id='-23242342'
 group_id='-'
+report_user_id=''
 
 session = vk.Session(access_token)
 api = vk.API(session)
 
+is_sent=False
+
 def ban_user(from_id,message_string,comment_id):
+    global today_ban_list
     userinfo=api.users.get(user_ids=from_id)
     with open("ban_list.txt", "a") as myfile:
-        myfile.write(str(userinfo)+':'+message_string+'\n')
+        myfile.write(time.strftime("%x %X")+' '+str(userinfo)+':'+message_string+'\n')
+    with open("today_ban_list.txt", "a") as todayfile:
+        todayfile.write(time.strftime("%x %X")+' '+str(userinfo)+':'+message_string+'\n')
     custom_group_id=group_id.replace('-','')
     api.groups.banUser(group_id=custom_group_id, user_id=from_id)
     api.wall.deleteComment(owner_id=group_id,comment_id=comment_id)
+    print('ban')
 
 
 def checkBadWords(from_id,message_string,check_comment_id):
@@ -31,7 +37,17 @@ def checkBadWords(from_id,message_string,check_comment_id):
             time.sleep(1)
 
 def getWall():
+    global is_sent
     while True:
+        if(int(time.strftime("%H"))==23 and int(time.strftime("%M"))>=0 and int(time.strftime("%M"))<10 and is_sent==False):
+            with open('today_ban_list.txt', 'r') as todbanfile:
+                today_ban_list=todbanfile.read()
+            api.messages.send(user_id=report_user_id,message=today_ban_list)
+            with open('today_ban_list.txt', 'w') as todbanfile:
+                todbanfile.write("")
+            is_sent=True
+        if(int(time.strftime("%H"))==23 and int(time.strftime("%M"))>10):
+        	is_sent=False
         wallist=api.wall.get(owner_id=group_id, count=count_of_posts)
         commentlist=[None]*count_of_posts
         for i in range(1,count_of_posts):
@@ -53,8 +69,9 @@ def getWall():
 def main():
     try:
         getWall()
-    except Exception, e:
+    except Exception as e:
         time.sleep(10)
+        print(e)
     finally:
         main()
 
